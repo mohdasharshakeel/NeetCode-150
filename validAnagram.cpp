f6e752b89d40986e7db65d7ef1bc422b3c2b4586
mohdasharshakeel <mohdasharshakeel@mail.com>
2026-02-02 23:07:51 +0530

    valid anagram with one map

--- validAnagram.cpp
+++ validAnagram.cpp
@@ -1,23 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
+// bool isAnagram(string s, string t){
+//     if(s.length()!= t.length())
+//     return false;
+
+//     unordered_map<char,int>countS;
+//     unordered_map<char,int>countT;
+
+//     for(int i = 0; i <s.length();i++){
+//         countS[s[i]]++;
+//         countT[t[i]]++;
+//     }
+
+//     for(auto it : countS){
+//             if (it.second != countT[it.first]) {
+//             return false;
+//         }
+
+
+//     }
+//     return true;
+// }
+
 bool isAnagram(string s, string t){
-    if(s.length()!= t.length())
-    return false;
+    unordered_map<char,int>freq;
 
-    unordered_map<char,int>countS;
-    unordered_map<char,int>countT;
-
-    for(int i = 0; i <s.length();i++){
-        countS[s[i]]++;
-        countT[t[i]]++;
+    if(s.length()!=t.length()){
+        return false;
     }
 
-    for(auto it : countS){
-            if (it.second != countT[it.first]) {
+    for(int i = 0; i < s.length();i++){
+        freq[s[i]]++;
+        freq[t[i]]--;
+    }
+    for(auto it : freq){
+        if(it.second != 0){
             return false;
         }
-
-
     }
     return true;
 }
@@ -34,3 +53,6 @@ int main(){
     }
     return 0;
 }
+
+
+//?  Valid Anagram check karne ke liye hum dono strings ki length pehle compare karte hain, phir ek hi map me pehli string ke characters ka count increase aur doosri string ke characters ka count decrease karte hain, jisse same characters ek-dusre ko cancel kar dete hain, aur end me agar sabhi characters ka final count zero hota hai to strings anagram hoti hain, warna nahi.
