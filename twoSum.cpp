c2f891dcf542f8ae0eeb77ead10964165ca8c4c4
mohdasharshakeel <mohdasharshakeel@mail.com>
2026-02-02 23:41:03 +0530

    two sum with hashmap ( unordered map)

---
+++ twoSum.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> checkSum(vector<int> nums, int target) {
+    unordered_map<int, int> preMap;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int diff = target - nums[i];
+
+        if (preMap.find(diff) != preMap.end()) {
+            return {preMap[diff], i}; // 2 indexes
+        }
+        preMap[nums[i]] = i;
+    }
+    return {}; // empty vector if not found
+}
+
+int main() {
+    vector<int> nums{1, 2, 3, 4, 5};
+    int target = 9;
+
+    vector<int> ans = checkSum(nums, target);
+
+    if (!ans.empty()) {
+        cout << ans[0] << " " << ans[1] << endl;
+    } else {
+        cout << "No pair found" << endl;
+    }
+}
