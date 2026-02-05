a56e218f203626d9369547c09e9932be3f6e41b8
mohdasharshakeel <mohdasharshakeel@mail.com>
2026-02-05 19:08:26 +0530

    ok

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,29 +1,61 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-vector<int> checkSum(vector<int> nums, int target) {
-    unordered_map<int, int> preMap;
+// vector<int> checkSum(vector<int> nums, int target) {
+//     unordered_map<int, int> preMap;
 
-    for (int i = 0; i < nums.size(); i++) {
+//     for (int i = 0; i < nums.size(); i++) {
+//         int diff = target - nums[i];
+
+//         if (preMap.find(diff) != preMap.end()) {
+//             return {preMap[diff], i}; // 2 indexes
+//         }
+//         preMap[nums[i]] = i;
+//     }
+//     return {}; // empty vector if not found
+// }
+
+// int main() {
+//     vector<int> nums{1, 2, 3, 4, 5};
+//     int target = 9;
+
+//     vector<int> ans = checkSum(nums, target);
+
+//     if (!ans.empty()) {
+//         cout << ans[0] << " " << ans[1] << endl;
+//     } else {
+//         cout << "No pair found" << endl;
+//     }
+// }
+
+
+ #include <bits/stdc++.h>
+ using namespace std;
+vector<int> checkSum(vector<int>nums,int target){
+    unordered_map<int,int>preVal;
+
+    for(int i = 0; i <nums.size();i++){
         int diff = target - nums[i];
-
-        if (preMap.find(diff) != preMap.end()) {
-            return {preMap[diff], i}; // 2 indexes
+        if(preVal.find(diff)!= preVal.end()){
+            return {preVal[diff],i};
         }
-        preMap[nums[i]] = i;
+        preVal[nums[i]] = i;
     }
-    return {}; // empty vector if not found
+
+    return {};
+
 }
+ int main(){
+    vector<int>nums{1,2,3,4,5,6,7};
+    int target = 12;
+   vector<int>ans = checkSum(nums,target);
+   if(!ans.empty()){
+       cout << " pair found at " << ans[0] << " " << ans[1] <<endl;
+   }else{
+    cout << " not found" << endl;
+   }
 
-int main() {
-    vector<int> nums{1, 2, 3, 4, 5};
-    int target = 9;
 
-    vector<int> ans = checkSum(nums, target);
 
-    if (!ans.empty()) {
-        cout << ans[0] << " " << ans[1] << endl;
-    } else {
-        cout << "No pair found" << endl;
-    }
-}
+    return 0;
+ }
